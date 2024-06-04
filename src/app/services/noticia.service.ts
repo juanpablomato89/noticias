67e2adaf1d7420f7325f06d98258a1cdd26ae857
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-06-04 14:56:43 -0400

    consulta a una API para obtener las noticias

--- src/app/services/noticia.service.ts
+++ src/app/services/noticia.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiaService {
 
-  constructor() { }
+  key = 'c6932a18b4a542bc90002c58bdb21801';
+  constructor(private http: HttpClient) { }
+
+  buscarNoticias(parametros: any): Observable<any> {
+
+    const URL = `https://newsapi.org/v2/top-headlines?country=${parametros.pais}&category=${parametros.categorias}&apiKey=${this.key}`;
+    return this.http.get(URL);
+  }
 }
